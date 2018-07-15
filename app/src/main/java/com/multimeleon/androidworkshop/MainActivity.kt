0f5da6473201ca2f5bd62a16d2d4eb2d321b9acb
Peter-John Welcome <pjwelcome@gmail.com>
2018-07-15 22:36:08 +0200

    Activity updated

--- app/src/main/java/com/multimeleon/androidworkshop/MainActivity.kt
+++ app/src/main/java/com/multimeleon/androidworkshop/MainActivity.kt
@@ -8,5 +8,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dashboard_recyclerView.adapter = DashboardAdapter(plants = it, clickListener = {
+            plantItemClicked(it)
+        })
+    }
+
+    private fun setupRecyclerView() {
+        dashboard_recyclerView.layoutManager = GridLayoutManager(this, 2)
+        this.bindData()
+    }
+
+    private fun plantItemClicked(plant: Plant) {
+        Intent(this, PlantDetailActivity::class.java).addData(mapOf(PlantId to plant.id)) {
+            navigateWith(this@DashboardActivity)
+        }
     }
 }
